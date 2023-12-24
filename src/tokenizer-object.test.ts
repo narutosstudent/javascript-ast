7dd16db3cdc7989b81c7f5daf5b0e94fe8a2cf7d
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-24 11:48:25 +0100

    lets parse primitives

--- src/tokenizer-object.test.ts
+++ src/tokenizer-object.test.ts
@@ -2,7 +2,7 @@ import { expect, it, describe } from 'vitest'
 
 import { tokenize } from './tokenizer'
 
-describe('Tokenizer: Object Literal Declaration', () => {
+describe.skip('Tokenizer: Object Literal Declaration', () => {
   describe('let declaration', () => {
     it('should tokenize a `let` declaration with an empty object', () => {
       const input = 'let obj = {};'
@@ -35,46 +35,46 @@ describe('Tokenizer: Object Literal Declaration', () => {
       ])
     })
 
-    //   it('should tokenize a `let` declaration with an object containing multiple properties', () => {
-    //     const input = 'let obj = { a: 1, b: 2 };'
-    //     const tokens = tokenize(input)
+    it('should tokenize a `let` declaration with an object containing multiple properties', () => {
+      const input = 'let obj = { a: 1, b: 2 };'
+      const tokens = tokenize(input)
 
-    //     expect(tokens).toEqual([
-    //       { type: 'Keyword', value: 'let' },
-    //       { type: 'Identifier', value: 'obj' },
-    //       { type: 'Operator', value: '=' },
-    //       { type: 'Punctuator', value: '{' },
-    //       { type: 'Identifier', value: 'a' },
-    //       { type: 'Punctuator', value: ':' },
-    //       { type: 'NumericLiteral', value: '1' },
-    //       { type: 'Punctuator', value: ',' },
-    //       { type: 'Identifier', value: 'b' },
-    //       { type: 'Punctuator', value: ':' },
-    //       { type: 'NumericLiteral', value: '2' },
-    //       { type: 'Punctuator', value: '}' },
-    //       { type: 'Punctuator', value: ';' },
-    //     ])
-    //   })
+      expect(tokens).toEqual([
+        { type: 'Keyword', value: 'let' },
+        { type: 'Identifier', value: 'obj' },
+        { type: 'Operator', value: '=' },
+        { type: 'Punctuator', value: '{' },
+        { type: 'Identifier', value: 'a' },
+        { type: 'Punctuator', value: ':' },
+        { type: 'NumericLiteral', value: '1' },
+        { type: 'Punctuator', value: ',' },
+        { type: 'Identifier', value: 'b' },
+        { type: 'Punctuator', value: ':' },
+        { type: 'NumericLiteral', value: '2' },
+        { type: 'Punctuator', value: '}' },
+        { type: 'Punctuator', value: ';' },
+      ])
+    })
 
-    //   it('should tokenize a `let` declaration with an object containing nested objects', () => {
-    // 	    const input = 'let obj = { a: { b: 1 } };'
-    //     const tokens = tokenize(input)
+    it('should tokenize a `let` declaration with an object containing nested objects', () => {
+      const input = 'let obj = { a: { b: 1 } };'
+      const tokens = tokenize(input)
 
-    //     expect(tokens).toEqual([
-    //       { type: 'Keyword', value: 'let' },
-    //       { type: 'Identifier', value: 'obj' },
-    //       { type: 'Operator', value: '=' },
-    //       { type: 'Punctuator', value: '{' },
-    //       { type: 'Identifier', value: 'a' },
-    //       { type: 'Punctuator', value: ':' },
-    //       { type: 'Punctuator', value: '{' },
-    //       { type: 'Identifier', value: 'b' },
-    //       { type: 'Punctuator', value: ':' },
-    //       { type: 'NumericLiteral', value: '1' },
-    //       { type: 'Punctuator', value: '}' },
-    //       { type: 'Punctuator', value: '}' },
-    //       { type: 'Punctuator', value: ';' },
-    //     ])
-    //   })
+      expect(tokens).toEqual([
+        { type: 'Keyword', value: 'let' },
+        { type: 'Identifier', value: 'obj' },
+        { type: 'Operator', value: '=' },
+        { type: 'Punctuator', value: '{' },
+        { type: 'Identifier', value: 'a' },
+        { type: 'Punctuator', value: ':' },
+        { type: 'Punctuator', value: '{' },
+        { type: 'Identifier', value: 'b' },
+        { type: 'Punctuator', value: ':' },
+        { type: 'NumericLiteral', value: '1' },
+        { type: 'Punctuator', value: '}' },
+        { type: 'Punctuator', value: '}' },
+        { type: 'Punctuator', value: ';' },
+      ])
+    })
   })
 })
