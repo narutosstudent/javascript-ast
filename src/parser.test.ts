73d2cfc30df9fb760fa836e75bb515bd7a7ac22b
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-25 14:47:11 +0100

    parser test

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -32,4 +32,164 @@ describe('Parser: Variable declaration', () => {
       ],
     })
   })
+
+  it.only('should parse a `let` declaration with a string', () => {
+    const tokens: Array<Token> = [
+      { type: 'Keyword', value: 'let' },
+      { type: 'Identifier', value: 'message' },
+      { type: 'Operator', value: '=' },
+      { type: 'StringLiteral', value: 'Hello' },
+      { type: 'Punctuator', value: ';' },
+    ]
+    const ast = parser(tokens)
+
+    expect(ast).toEqual({
+      type: 'Program',
+      body: [
+        {
+          type: 'VariableDeclaration',
+          declarations: [
+            {
+              type: 'VariableDeclarator',
+              id: { type: 'Identifier', name: 'message' },
+              init: { type: 'Literal', value: 'Hello' },
+            },
+          ],
+          kind: 'let',
+        },
+      ],
+    })
+  })
+
+  it('should parse a `let` declaration with a boolean', () => {
+    const tokens: Array<Token> = [
+      { type: 'Keyword', value: 'let' },
+      { type: 'Identifier', value: 'isActive' },
+      { type: 'Operator', value: '=' },
+      { type: 'BooleanLiteral', value: 'true' },
+      { type: 'Punctuator', value: ';' },
+    ]
+    const ast = parser(tokens)
+
+    expect(ast).toEqual({
+      type: 'Program',
+      body: [
+        {
+          type: 'VariableDeclaration',
+          declarations: [
+            {
+              type: 'VariableDeclarator',
+              id: { type: 'Identifier', name: 'isActive' },
+              init: { type: 'Literal', value: true },
+            },
+          ],
+          kind: 'let',
+        },
+      ],
+    })
+  })
+
+  it('should parse a `let` declaration with a null', () => {
+    const tokens: Array<Token> = [
+      { type: 'Keyword', value: 'let' },
+      { type: 'Identifier', value: 'item' },
+      { type: 'Operator', value: '=' },
+      { type: 'NullLiteral', value: 'null' },
+      { type: 'Punctuator', value: ';' },
+    ]
+    const ast = parser(tokens)
+
+    expect(ast).toEqual({
+      type: 'Program',
+      body: [
+        {
+          type: 'VariableDeclaration',
+          declarations: [
+            {
+              type: 'VariableDeclarator',
+              id: { type: 'Identifier', name: 'item' },
+              init: { type: 'Literal', value: null },
+            },
+          ],
+          kind: 'let',
+        },
+      ],
+    })
+  })
+
+  it('should parse multiple `let` declarations with different primitive types', () => {
+    const tokens: Array<Token> = [
+      { type: 'Keyword', value: 'let' },
+      { type: 'Identifier', value: 'x' },
+      { type: 'Operator', value: '=' },
+      { type: 'NumericLiteral', value: '5' },
+      { type: 'Punctuator', value: ';' },
+      { type: 'Keyword', value: 'let' },
+      { type: 'Identifier', value: 'message' },
+      { type: 'Operator', value: '=' },
+      { type: 'StringLiteral', value: 'Hello' },
+      { type: 'Punctuator', value: ';' },
+      { type: 'Keyword', value: 'let' },
+      { type: 'Identifier', value: 'isActive' },
+      { type: 'Operator', value: '=' },
+      { type: 'BooleanLiteral', value: 'true' },
+      { type: 'Punctuator', value: ';' },
+      { type: 'Keyword', value: 'let' },
+      { type: 'Identifier', value: 'item' },
+      { type: 'Operator', value: '=' },
+      { type: 'NullLiteral', value: 'null' },
+      { type: 'Punctuator', value: ';' },
+    ]
+    const ast = parser(tokens)
+
+    expect(ast).toEqual({
+      type: 'Program',
+      body: [
+        {
+          type: 'VariableDeclaration',
+          declarations: [
+            {
+              type: 'VariableDeclarator',
+              id: { type: 'Identifier', name: 'x' },
+              init: { type: 'Literal', value: 5 },
+            },
+          ],
+          kind: 'let',
+        },
+        {
+          type: 'VariableDeclaration',
+          declarations: [
+            {
+              type: 'VariableDeclarator',
+              id: { type: 'Identifier', name: 'message' },
+              init: { type: 'Literal', value: 'Hello' },
+            },
+          ],
+          kind: 'let',
+        },
+        {
+          type: 'VariableDeclaration',
+          declarations: [
+            {
+              type: 'VariableDeclarator',
+              id: { type: 'Identifier', name: 'isActive' },
+              init: { type: 'Literal', value: true },
+            },
+          ],
+          kind: 'let',
+        },
+        {
+          type: 'VariableDeclaration',
+          declarations: [
+            {
+              type: 'VariableDeclarator',
+              id: { type: 'Identifier', name: 'item' },
+              init: { type: 'Literal', value: null },
+            },
+          ],
+          kind: 'let',
+        },
+      ],
+    })
+  })
 })
